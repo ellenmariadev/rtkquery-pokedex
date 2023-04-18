92e9417a582d7e3f92cf07195fc4598acb379a5c
ellenmariadev <ellenmariadev@gmail.com>
2023-04-18 03:33:00 -0300

    test(config): setup test environment Dependencies: redux-mock-store eslint-plugin-jest

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   testEnvironment: "jest-environment-jsdom",
   setupFilesAfterEnv: ["<rootDir>/.jest/setup-tests.js"],
   moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
     "\\.(gif|ttf|eot|svg|png)$": "<rootDir>/.jest/mocks/fileMock.js",
     "\\.(css|less|sass|scss)$": "identity-obj-proxy",
   },
--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,10 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+  },
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
   },
