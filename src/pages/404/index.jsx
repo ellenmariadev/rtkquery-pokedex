196440a1f804995beb28ae3e5e377a4f971ed859
ellenmariadev <ellenmariadev@gmail.com>
2023-04-18 03:11:58 -0300

    feat(style): styling error page

--- src/pages/404/index.jsx
+++ src/pages/404/index.jsx
@@ -1,3 +1,31 @@
+import { Button, Heading, Container, Text, Center } from "@chakra-ui/react";
+import { Link as RouterLink } from "react-router-dom";
+
 export const ErrorPage = () => {
-  return <div>ErrorPage</div>;
+  return (
+    <main>
+      <Container
+        display="grid"
+        height="70vh"
+        placeContent="center"
+        textAlign="center"
+      >
+        <Heading as="h1" fontSize="8rem">404</Heading>
+        <Text letterSpacing={1} p={5}>
+          Oops! Página não encontrada.
+        </Text>
+        <Center>
+          <Button
+            colorScheme="red"
+            size="md"
+            width="150px"
+            as={RouterLink}
+            to="/"
+          >
+            Voltar
+          </Button>
+        </Center>
+      </Container>
+    </main>
+  );
 };
